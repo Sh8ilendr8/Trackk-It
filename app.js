32e2f83dd35742a83f58b3519931932af6616aa0
Ayush Goel <ayushgoel2804@gmail.com>
2022-04-21 00:47:59 +0530

    Remove favicon

--- app.js
+++ app.js
@@ -941,10 +941,11 @@ newContact.save(function(err){
 });
 
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-
-
-
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log("Server started on port 3000.");
 });
